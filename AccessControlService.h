b0d996a441d36506f00b10845050b8d998ca084f
lesegot <lesegot2005@gmail.com>
2026-09-24 00:28:07 +0200

    Added function definition in AccessControlService.h

---
+++ AccessControlService.h
@@ -0,0 +1,28 @@
+#ifndef ACCESSCONTROLSERVICE_H
+#define ACCESSCONTROLSERVICE_H
+
+#include "Colleague.h"
+#include <string>
+
+class IMediator;
+
+class AccessControlService : public Colleague
+{
+    private:
+        std::string event;
+        std::string payload;
+
+    public:
+        AccessControlService(IMediator* mediator);
+
+        virtual std::string getEvent() override;
+        virtual std::string getPayload() override;
+        virtual void handlEvent(std::string payload) override;
+
+        void restrictDoors(std::string location);
+        void unlockEmergencyExits(std::string location);
+
+        ~AccessControlService() override;
+};
+
+#endif /*ACCESSCONTROLSERVICE_H*/
