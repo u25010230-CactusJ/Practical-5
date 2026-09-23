0149f3a790bd151f1eecdf84b5bbffaa31806261
lesegot <lesegot2005@gmail.com>
2026-09-24 00:27:08 +0200

    Added function definition in Colleague.h

---
+++ Colleague.h
@@ -0,0 +1,23 @@
+#ifndef COLLEAGUE_H
+#define COLLEAGUE_H
+
+class IMediator;
+#include <string>
+
+class Colleague
+{
+    protected:
+        IMediator* mediator;
+
+    public:
+        Colleague(IMediator* mediator);
+        void changed();
+
+        virtual std::string getEvent() = 0;
+        virtual std::string getPayload() = 0;
+
+        virtual void handlEvent(std::string payload) = 0;
+        virtual ~Colleague() = default;
+};
+
+#endif /*COLLEAGUE_H*/
