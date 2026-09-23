6ea32353cb42d3b1d554710ae29858a0de62e874
lesegot <lesegot2005@gmail.com>
2026-09-24 00:26:04 +0200

    Added function definition in CampusMediator.h

---
+++ CampusMediator.h
@@ -0,0 +1,20 @@
+#ifndef CAMPUSMEDIATOR_H
+#define CAMPUSMEDIATOR_H
+
+#include "IMediator.h"
+#include <vector>
+
+class CampusMediator : public IMediator
+{
+    private:
+        std::vector<Colleague*> colleagues;
+
+    public:
+        void registerColleague(Colleague* newCol);
+        void deregisterColleague(Colleague* newCol);
+        virtual void notify(Colleague* sender) override;
+        
+        ~CampusMediator() override;
+};
+
+#endif /*CAMPUSMEDIATOR_H*/
