faac5675d846dab8218c35756e82973351ad4968
u25010230-CactusJ <u25010230@tuks.co.za>
2026-09-24 18:32:24 +0200

    Updated

--- ModernNotifier.h
+++ ModernNotifier.h
@@ -2,14 +2,17 @@
 #define MODERN_NOTIFIER_H
 
 #include <string>
+#include "Colleague.h"
 using namespace std;
 
 
 // target interface expected by modern CG subsystems
-class ModernNotifier {
+class ModernNotifier : public Colleague {
 public:
 virtual ~ModernNotifier() = default; // default destructor
 virtual void sendAlert( const string& zone , const string& message ) = 0; // pure virtual
+virtual void cancelAlert( string location);
+virtual bool handleEvent( const string& event, const string& payload);
 
 };
 
