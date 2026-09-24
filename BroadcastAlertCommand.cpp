221feffc3a83b9f333f3b68879c20bcb6f13694a
lesegot <lesegot2005@gmail.com>
2026-09-24 11:17:19 +0200

    Implemented BroadcastAlertCommand

---
+++ BroadcastAlertCommand.cpp
@@ -0,0 +1,46 @@
+#include "BroadcastAlertCommand.h"
+#include "ModernNotifier.h"
+
+BroadcastAlertCommand::BroadcastAlertCommand(ModernNotifier* notifier, std::string location, std::string msg)
+    : notifier(notifier), location(location), msg(msg)
+{}
+
+void BroadcastAlertCommand::execute()
+{
+    this->notifier->sendAlert(this->location, this->msg);
+}
+
+void BroadcastAlertCommand::undo()
+{
+
+}
+
+std::string BroadcastAlertCommand::getLocation()
+{
+    return this->location;
+}
+
+void BroadcastAlertCommand::setLocation(std::string location)
+{
+    this->location = location;
+}
+
+std::string BroadcastAlertCommand::getMSG()
+{
+    return this->msg;
+}
+
+void BroadcastAlertCommand::setMSG(std::string msg)
+{
+    this->msg = msg;
+}
+
+ModernNotifier* BroadcastAlertCommand::getReceiver()
+{
+    return this->notifier;
+}
+
+void BroadcastAlertCommand::setReceiver(ModernNotifier* notifier)
+{
+    this->notifier = notifier;
+}
