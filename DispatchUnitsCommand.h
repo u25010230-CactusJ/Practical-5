18da55dcc505c1c1288a48cb1e9ba2a3f5866d58
lesegot <lesegot2005@gmail.com>
2026-09-24 11:28:25 +0200

    Added function definition

---
+++ DispatchUnitsCommand.h
@@ -0,0 +1,29 @@
+#ifndef DISPATCHUNITSCOMMAND_H
+#define DISPATCHUNITSCOMMAND_H
+
+#include "ICommand.h"
+#include <string>
+
+class SecurityService;
+class IncidentContext;
+
+class DispatchUnitsCommand : public ICommand
+{
+    private:
+        SecurityService* security;
+        IncidentContext* incident;
+        bool executed;
+        
+    public:
+        DispatchUnitsCommand(SecurityService* security, IncidentContext* incident);
+
+        virtual void execute() override;
+        virtual void undo() override;
+
+        SecurityService* getReceiver();
+        void setReceiver(SecurityService* security);
+        
+        ~DispatchUnitsCommand() override = default;
+};
+
+#endif /*DISPATCHUNITSCOMMAND_H*/
